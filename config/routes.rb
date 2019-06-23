a4e12e4f3d9d2d9cd126ba5f749ebf68229f2f00
github username <github email address>
2019-06-22 21:20:40 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  resources :users
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+
+
+  root 'sessions#index'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
