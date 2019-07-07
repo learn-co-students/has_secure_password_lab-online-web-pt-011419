e1acd97420ad627e7864e3279f2fbaa38309997f
imaglazer <avigayilglazer@gmail.com>
2019-07-07 16:46:24 +0300

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,9 @@ class UsersController < ApplicationController
   
   def create
     @user = User.create(user_params)
-
+    
     if @user.save
+      session[:user_id] = @user.id
       redirect_to "/"
     else
       redirect_to new_user_path
